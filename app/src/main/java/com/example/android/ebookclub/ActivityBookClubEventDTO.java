0cebb6f34b146768d2ae9350f7774af533ffc34c
Vidhyashree <vd896908@dal.ca>
2018-04-08 19:13:04 -0700

    dto changes

--- app/src/main/java/com/example/android/ebookclub/ActivityBookClubEventDTO.java
+++ app/src/main/java/com/example/android/ebookclub/ActivityBookClubEventDTO.java
@@ -13,41 +13,59 @@ public class ActivityBookClubEventDTO {
     public ActivityBookClubEventDTO(){}
 
     public ActivityBookClubEventDTO(String id, String eventName, String eventLocation, String eventDescription) {
+
         this.id = id;
         this.eventName = eventName;
         this.eventLocation = eventLocation;
         this.eventDescription = eventDescription;
+
     }
 
     public String getEventName() {
+
         return eventName;
+
     }
 
     public void setEventName(String eventName) {
+
         this.eventName = eventName;
+
     }
 
     public String getEventLocation() {
+
         return eventLocation;
+
     }
 
     public void setEventLocation(String eventLocation) {
+
         this.eventLocation = eventLocation;
+
     }
 
     public String getEventDescription() {
+
         return eventDescription;
+
     }
 
     public void setEventDescription(String eventDescription) {
+
         this.eventDescription = eventDescription;
+
     }
 
     public String getId() {
+
         return id;
+
     }
 
     public void setId(String id) {
+
         this.id = id;
+
     }
 }
